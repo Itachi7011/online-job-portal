6ad472dff9c775a27e85261bafc342fba509529c
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-06 23:03:18 +0530

    Update EmpProfile.js

--- client/src/Components/Employee/EmpProfile.js
+++ client/src/Components/Employee/EmpProfile.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 
 export default function EmpProfile() {
-
       
     const [user, setUser] = useState("")
     const callProfilePage = async () => {
